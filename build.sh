a068f94288d1f45ef2e703b8f1f31834ee0afcb0
Valentino Medimorec <valentino@vallic.com>
2022-09-06 20:57:29 +0200

    Adjust proper path

--- build.sh
+++ build.sh
@@ -49,10 +49,13 @@ chmod +x $APP_DIRECTORY/AppRun
 echo "==> Extract defaults for AppImage"
 # Add defaults which we need for proper app image. Desktop files, icons.
 cp code.desktop $APP_DIRECTORY/.
-cp $BIN_DIRECTORY/resources/app/resources/linux/code.png $APP_DIRECTORY/code.png
-convert code.png -resize 512x512 $APP_DIRECTORY/usr/share/icons/hicolor/512x512/apps/code.png
-convert code.png -resize 256x256 $APP_DIRECTORY/usr/share/icons/hicolor/256x256/apps/code.png
-convert code.png -resize 128x128 $APP_DIRECTORY/usr/share/icons/hicolor/128x128/apps/code.png
+cd $APP_DIRECTORY
+cp $BIN_DIRECTORY/resources/app/resources/linux/code.png code.png
+convert code.png -resize 512x512 usr/share/icons/hicolor/512x512/apps/code.png
+convert code.png -resize 256x256 usr/share/icons/hicolor/256x256/apps/code.png
+convert code.png -resize 128x128 usr/share/icons/hicolor/128x128/apps/code.png
+
+cd ..
 
 # Get GitHub user and repo
 GH_USER="$( echo "$GITHUB_REPOSITORY" | grep -o ".*/" | head -c-2 )"
